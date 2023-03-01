56a7c2019cda31a415da5e6b0e0f7243a67516af
lucasmm56 <lucasmm56@gmail.com>
2023-03-01 20:44:40 -0300

    alterações no js

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ var apiKey = 'apikey=6cdda3fb';
 var xhr = new XMLHttpRequest();
 
 function findFilm(){
-    // xhr.abort()
     if(!title.value){
         alert('O campo titulo é obrigatório');
         return false;
     }else{
         xhr.open('GET', `https://www.omdbapi.com/?t=${title.value}&y=${year.value}&${apiKey}`, true);
         xhr.onload = function(){
+            
             if(xhr.status === 200){
                 var data = JSON.parse(xhr.responseText);
-                console.log(data);
+                if(data['Response'] === 'False'){
+                    alert(`${data['Error']}`);
+                    return false;
+                }
                 document.querySelector('.title-film').innerHTML = `${data['Title']}`;
                 document.querySelector('.img-film').src = `${data['Poster']}`;
                 document.querySelector('.year-film').innerHTML = `Ano: ${data['Year']}`;
@@ -22,18 +25,18 @@ function findFilm(){
                 document.querySelector('.duration').innerHTML = `Duração: ${data['Runtime']}`;
                 document.querySelector('.avaliation').innerHTML = `Avaliação: ${data['imdbRating']}`;
                 document.querySelector('.votes').innerHTML = `Votos: ${data['imdbVotes']}`;
-                alterCss(document.querySelector('.duration'));
-                alterCss(document.querySelector('.avaliation'));
-                alterCss(document.querySelector('.votes'));
+                alterCss(document.querySelectorAll(['.duration', '.avaliation', '.votes']));
             }else{
                 console.error('Falha na conexão com API')
             }
-        }   
+        }
         xhr.send();   
     }
 }
 
 function alterCss(value){
-    value.style.border = '1px solid white';
+    for(let i = 0; i< value.length; i++){
+        value[i].style.border = '1px solid white';
+    }
 }
 
